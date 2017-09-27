a21ab55cf28a335509ba75c6d7fec880e5866d97
Noel Delgado <noeldelgadom@myhost.local>
2017-09-27 14:59:43 -0500

    5. adding firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAwjoSJQNbPZ11XTXqZp5j9tZzqr1Xlfuo',
+  authDomain: 'pepper-4d5f3.firebaseapp.com',
+  databaseURL: 'https://pepper-4d5f3.firebaseio.com',
+  projectId: 'pepper-4d5f3',
+  storageBucket: 'pepper-4d5f3.appspot.com',
+  messagingSenderId: '764886278926'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
