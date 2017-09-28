6c183d2ffe21c0e611ff6e13cc1aa83b201359f2
Noel Delgado <noeldelgadom@myhost.local>
2017-09-28 10:01:02 -0500

    26 & 27. working with the current user, requesting additional facebook permissions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { Http } from '@angular/http';
+import { AuthMethods, AuthProviders } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
@@ -10,40 +12,34 @@ import 'rxjs/add/operator/take';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  cuisines: FirebaseListObservable<any[]>;
-  restaurants: Observable<any[]>;
-  exists;
+  displayName;
+  photoURL;
 
-  constructor(private af: AngularFire) {}
+  constructor(private af: AngularFire, private http: Http) {}
 
   ngOnInit() {
-    this.cuisines = this.af.database.list('/cuisines', {
-      query: { orderByKey: true }
-    });
-    this.restaurants = this.af.database.list('/restaurants', {
-      query: {
-        orderByChild: 'rating',
-        equalTo: 5
+    this.af.auth.subscribe(authState => {
+      if (!authState) {
+        this.displayName = null;
+        this.photoURL = null;
+        return;
       }
-    })
-      .map(restaurants => {
-        restaurants.map(restaurant => {
-          restaurant.featureTypes = [];
-          // tslint:disable-next-line:forin
-          for (const f in restaurant.features) {
-            restaurant.featureTypes.push(this.af.database.object('/features/' + f));
-          }
-          restaurant.cuisineType = this.af.database.object('/cuisines/' + restaurant.cuisine);
-        });
-        return restaurants;
-      });
-
-    this.exists = this.af.database.object('/restaurants/1/features/1');
-    this.exists.take(1).subscribe(x => {
-      // tslint:disable-next-line:curly
-      if (x && x.$value) console.log('Exists');
-      // tslint:disable-next-line:curly
-      else console.log('Not Exists');
+      this.displayName = authState.auth.displayName;
+      this.photoURL = authState.auth.photoURL;
     });
+  }
+
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup,
+      scope: ['public_profile', 'user_friends']
+    }).then(authState => {
+      console.log('After Login', authState);
+    });
+  }
+
+  logout() {
+    this.af.auth.logout();
   }
 }
